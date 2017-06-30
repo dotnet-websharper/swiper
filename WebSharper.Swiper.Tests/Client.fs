3a1af2cc3da8397fb8f22ce51d0bcf88d6ce6978
Ádám Abonyi-Tóth <atadi96@gmail.com>
2017-06-30 18:03:57 +0200

    Added tests fort SlideTo and SlidePrev/SlideNext

--- WebSharper.Swiper.Tests/Client.fs
+++ WebSharper.Swiper.Tests/Client.fs
@@ -26,6 +26,29 @@ module Client =
                 equalMsg (swiper.ActiveIndex) 0 "The active index is 0 at the start."
             }
 
+            Test "SlideTo test" {
+                swiper.SlideTo 0
+                equalMsg swiper.ActiveIndex 0 "SlideTo 0"
+                swiper.SlideTo 4
+                equalMsg swiper.ActiveIndex 4 "SlideTo 4"
+                swiper.SlideTo 3
+                equalMsg swiper.ActiveIndex 3 "SlideTo 3"
+                swiper.SlideTo 0
+                equalMsg swiper.ActiveIndex 0 "SlideTo 0"
+            }
+
+            Test "Swiping around the start of the slides" {
+                swiper.SlidePrev()
+                equalMsg swiper.ActiveIndex 0 "Can not swipe left from the first slide"
+                swiper.SlideTo(0)
+                swiper.SlideNext()
+                equalMsg swiper.ActiveIndex 1 "Slide right from the first slide"
+                swiper.SlideTo 0
+                equalMsg swiper.ActiveIndex 0 "Slide to id 0"
+            }
+
+
+
         }
 
 #if ZAFIR

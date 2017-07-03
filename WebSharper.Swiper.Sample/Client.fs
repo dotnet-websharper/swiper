1f3c53da7d87b05d8b27ab1c59983ae7c5a9796a
Ádám Abonyi-Tóth <atadi96@gmail.com>
2017-07-03 16:59:16 +0200

    Done with the sample code

--- WebSharper.Swiper.Sample/Client.fs
+++ WebSharper.Swiper.Sample/Client.fs
@@ -12,10 +12,45 @@ open WebSharper.UI.Next.Templating
 module Client =
     // The templates are loaded from the DOM, so you just can edit index.html
     // and refresh your browser, no need to recompile unless you add or remove holes.
-    
+
     [<SPAEntryPoint>]
     let Main () =
-        let swiper = new WebSharper.Swiper.Swiper(".swiper-container")
-        text ("active: " + string swiper.ActiveIndex)
-        |>
-        Doc.RunAppendById "main"
+        let currentSlideIndex = Var.Create 0
+
+        let config = WebSharper.Swiper.SwipeParameters()
+        config.OnSlideChangeStart <- (fun swiper -> Var.Set currentSlideIndex swiper.ActiveIndex)
+
+        let swiper =
+            new WebSharper.Swiper.Swiper(
+                ".swiper-container",
+                config
+            )
+
+        let answerButtons =
+            Doc.Concat [
+                text "Pick an answer: "
+                Doc.Button "A" [] (fun () -> ())
+                Doc.Button "B" [] (fun () -> ())
+                Doc.Button "C" [] (fun () ->
+                    swiper.UnlockSwipeToNext()
+                    swiper.SlideNext()
+                    let newSlide () =
+                        divAttr [attr.``class`` "swiper-slide"] [
+                            text "You have finished the tutorial!"
+                        ]
+                    swiper.AppendSlide( (newSlide()).Dom )
+                )
+            ]
+
+        currentSlideIndex.View
+        |> View.Map (fun index ->
+            let questionSlideIndex = 8
+            if index = questionSlideIndex then
+                swiper.LockSwipeToNext()
+                answerButtons
+            else
+                swiper.UnlockSwipeToNext()
+                Doc.Empty
+        )
+        |> Doc.EmbedView
+        |> Doc.RunAppendById "container"

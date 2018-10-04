32bf3b392760ba1285feaeabca55973d598add62
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-10-04 15:02:37 +0200

    Update license info

--- WebSharper.Swiper/Main.fs
+++ WebSharper.Swiper/Main.fs
@@ -1,3 +1,22 @@
+// $begin{copyright}
+//
+// This file is part of WebSharper
+//
+// Copyright (c) 2008-2018 IntelliFactory
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// $end{copyright}
 namespace WebSharper.Swiper.Extension
 
 open WebSharper
@@ -268,7 +287,7 @@ module Definition =
                     "observer", T<bool>
                     "observeParents", T<bool>
                     //Breakpoints
-                    "breakpoints", T<obj> //KÉRDÉS
+                    "breakpoints", T<obj> //Kï¿½RDï¿½S
                     //Callbacks
                     "runCallbacksOnInit", T<bool>
                     "onInit", Callback
